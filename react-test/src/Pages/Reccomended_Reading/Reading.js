257a268df909c6a1d462e7804730503c06ca2dd0
elliercottrell <85800129+elliercottrell@users.noreply.github.com>
2022-11-05 18:42:52 +0000

    updated react

---
+++ react-test/src/Pages/Reccomended_Reading/Reading.js
@@ -0,0 +1,40 @@
+import logo from "../../Layout/Logo/HBA_logo.svg";
+import "../HomePage/App.css";
+import { Link } from "react-router-dom";
+import FooterLinks from "../../Layout/Footer/Footer";
+import ReadingCarousel from "./ReadingCarousel";
+import "bootstrap/dist/css/bootstrap.min.css";
+import ReactPlayer from "react-player";
+
+function Reading() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+      <div className="ReadingCarousel">
+        <ReadingCarousel></ReadingCarousel>
+      </div>
+      <div
+        className="ReactPlayer"
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <ReactPlayer url="https://www.youtube.com/watch?v=NUskHl7APHE" />
+      </div>
+
+      <p>Some reading links</p>
+      <Link to="/" className="link">
+        <button className="backButton">Back</button>
+      </Link>
+      <div className="App-footer">
+        <FooterLinks />
+      </div>
+    </div>
+  );
+}
+
+export default Reading;
---
+++ react-test/src/index.js
@@ -0,0 +1,66 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./Pages/HomePage/App";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Insulin from "./Pages/Insulin/Insulin";
+import FoodCalculator from "./Pages/FoodCalculator";
+import Reading from "./Pages/Reccomended_Reading/Reading";
+import About from "./Pages/About";
+import ContactUs from "./Pages/ContactUs";
+import Cookies from "./Pages/CookiePolicy";
+import TermsConditions from "./Pages/TCS";
+import Privacy from "./Pages/Privacy";
+import InsulinHistory from "./Pages/Insulin/InsulinHistory";
+import Parents from "./Pages/Parents";
+import TopBar from "./Layout/TopBar/TopBar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import SignIn from "./Pages/Authentication/SignIn";
+import SignUp from "./Pages/Authentication/SignUp";
+import UpdateInsulinDose from "./Pages/Insulin/UpdateInsulinDose";
+import InsulinCalculator from "./Pages/InsulinCalculator/InsulinCalculator";
+
+const Routing = () => {
+  return (
+    <Router>
+      <TopBar />
+      {/* <nav>
+        <Link to="/">Home</Link>
+        <br />
+        <Link to="insulin">Insulin</Link>
+        <br />
+        <Link to="InsulinCalculator">InsulinCalculator</Link>
+        <br />
+        <Link to="food">Food</Link>
+        <br />
+        <Link to="reading">Reading</Link>
+      </nav> */}
+      <div className="app">
+        <Routes>
+          <Route path="/" index element={<App />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/Parents" element={<Parents />} />
+          <Route path="/insulin-calculator" element={<InsulinCalculator />} />
+          <Route path="/insulin-history" element={<InsulinHistory />} />
+          <Route path="/insulin" element={<Insulin />} />
+          <Route path="/insulin-dose" element={<UpdateInsulinDose />} />
+          <Route path="/food-calculator" element={<FoodCalculator />} />
+          <Route path="/reading" element={<Reading />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contactus" element={<ContactUs />} />
+          <Route path="/cookiespolicy" element={<Cookies />} />
+          <Route path="/tcs" element={<TermsConditions />} />
+          <Route path="/privacy" element={<Privacy />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Routing />
+  </React.StrictMode>
+);
